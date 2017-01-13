2007c1d3cb9553356bb0b59e69cdacc552883d20
Plailect <plailect@gmail.com>
2017-01-12 21:27:58 -0500

    2xrsa

--- autostyle.js
+++
@@ -1,9 +0,0 @@
-var fileref=document.createElement('link')
-fileref.setAttribute('rel', 'stylesheet')
-fileref.setAttribute('type', 'text/css')
-fileref.setAttribute('href', './web.css')
-if (navigator.platform.indexOf('Nintendo 3DS') > -1){
-    fileref.setAttribute('href', '/.3ds.css')
-    window.setInterval(function(){window.scrollTo(40,220);},50);
-}
-document.getElementsByTagName('head')[0].appendChild(fileref)
